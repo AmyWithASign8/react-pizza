898e61e1cd57b10d3880896b162ad75b3f3b0163
DanilFrontend <danilstorage@gmail.com>
2022-06-09 00:02:49 +0500

    Add adaptive and interacted with json, use fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
 import { Header } from "./components";
-import React from "react";
+import React, { useEffect } from "react";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router";
 
 function App() {
+
+	const [pizzas, setPizzas] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+			setPizzas(json.pizzas);
+		})
+	}, []);
+
+	console.log(pizzas);
 	return (
 		<div>
 			<div className="wrapper">
 				<Header />
 				<div className="content">
 					<Routes>
-						<Route path="Home" element={<Home />} exact />
-						<Route path="Cart" element={<Cart />} exact />
+						<Route path="/Home" element={<Home items={pizzas}/>} exact />
+						<Route path="/Cart" element={<Cart />} exact />
 					</Routes>
 				</div>
 			</div>
